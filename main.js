9a963de09b6a17fc8b00be84ecbf356b501a90ff
TungNguyen6688 <nguyenxuantung1221@gmail.com>
2019-08-04 01:17:20 +0700

    init project

---
+++ main.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+
+    // $('.products').isotope({
+    //     // options
+    //     itemSelector: '.product',
+    //     layoutMode: 'masonry'
+    // });
+
+    // init Isotope
+    var $products = $('.products').isotope({
+        itemSelector: '.product',
+        layoutMode: 'masonry'
+    });
+
+    // layout Isotope after each image loads
+    $products.imagesLoaded().progress(function () {
+        $products.isotope('layout');
+    });
+
+    $('.category a').click(function(){
+        $('.category a').removeClass('active');
+        $(this).addClass('active');
+
+        var href = $(this).attr('href');
+
+        $products.isotope({filter: href});
+        
+        return false;
+    });
+})
